d9aedeef0c384e447e030b3e677c52f59e64d6a3
Rohini_Immanni <Rohini_Immanni@HYDHTC354133D.ad.infosys.com>
2017-05-12 17:20:45 +0530

    update 3 for moduleA

--- NewProject/src/PackageNew/ClassA.java
+++ NewProject/src/PackageNew/ClassA.java
@@ -7,6 +7,7 @@ public class ClassA {
 System.out.println("This is CLass A");
 System.out.println("this is class a code update 1");
 System.out.println("this is class a code update 2");
+System.out.println("this is class a code update 3");
 	}
 
 }
