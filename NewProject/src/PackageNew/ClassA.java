d9b9361b9fe6d3e949b42961c0e9c000771698ac
rohini199 <immanni.rohini@gmail.com>
2017-05-12 16:34:40 +0530

    added a display in classA

--- NewProject/src/PackageNew/ClassA.java
+++ NewProject/src/PackageNew/ClassA.java
@@ -5,6 +5,7 @@ public class ClassA {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("This is CLass A");
+		System.out.println("This is CLass A1");
 	}
 
 }
