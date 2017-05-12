d8b6ee6884b4d2c0190416f3e821c92cbe957f1f
Rohini_Immanni <Rohini_Immanni@HYDHTC354133D.ad.infosys.com>
2017-05-12 16:21:00 +0530

    This is a new commit

---
+++ NewProject/src/PackageNew/ClassA.java
@@ -0,0 +1,10 @@
+package PackageNew;
+
+public class ClassA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is CLass A");
+	}
+
+}
---
+++ NewProject/src/PackageNew/ClassB.java
@@ -0,0 +1,10 @@
+package PackageNew;
+
+public class ClassB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is Class B");
+	}
+
+}
