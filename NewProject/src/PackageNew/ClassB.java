b84e2dfc14fee5a3828aa15274b3f935aac7cf94
Rohini_Immanni <Rohini_Immanni@HYDHTC354133D.ad.infosys.com>
2017-05-12 17:22:10 +0530

    update 2 for module B

--- NewProject/src/PackageNew/ClassB.java
+++ NewProject/src/PackageNew/ClassB.java
@@ -6,6 +6,7 @@ public class ClassB {
 		// TODO Auto-generated method stub
 System.out.println("This is Class B");
 System.out.println("this is class B code update 1");
+System.out.println("this is class B code update 2");
 	}
 
 }
